939db027451f61cde6e484e49e970667139919a4
simplefly2 <1419020389@qq.com>
2024-09-02 23:42:28 +0800

    add reduceBase

---
+++ layerNorm/kernel.cu
@@ -0,0 +1,270 @@
+﻿#include "stdio.h"
+#include "stdlib.h"
+#include <math.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+# define CHECK(func) do \
+{ \
+	cudaError_t err = func; \
+	if (err != cudaSuccess) \
+	{                 \
+		printf("error: %s, %d \n", cudaGetErrorString(err), __LINE__); \
+	} \
+} while(0)
+
+
+
+#define M 1024
+#define N 768
+
+#define BLOCK_SIZE 1024
+#define WARP_SIZE 32
+
+// input, shape(M, N)
+// output, shape(M, N)
+// mean = sum(ni) / N
+// std = sqrt(sum((mean-xi)*(mean-xi)) / N)
+
+template <typename T>
+void cpu_layernorm(T * input, T * output, T g, T b, int m, int n)
+{
+	for (unsigned i = 0; i < m; ++i)
+	{
+		T mean = 0.0;
+		T std = 0.0;
+
+		// mean
+		T mean_sum = (T)0.0;
+		for (unsigned j = 0; j < n; ++j)
+		{
+			mean_sum += input[i * n + j];
+		}
+
+		mean = mean_sum / n;
+
+		// std
+		T std_sum = (T)0.0;
+		for (unsigned j = 0; j < n; ++j)
+		{
+			std_sum += ((mean - input[i * n + j]) * (mean - input[i * n + j]));
+		}
+
+		std = sqrtf(std_sum / n);
+
+		// output: (xi-mean) / std;
+		for (unsigned k = 0; k < n; ++k)
+		{
+			output[i * n + k] = g*(input[i * n + k] - mean) / (std + 1e-6) + b;
+		}
+	}
+}
+
+
+// 每行的归一化是独立的，所以可以并行
+// 一个线程处理一行数据
+template <typename T>
+__global__ void layerNorm_kernel(T* input, T* output, T g, T b, int m, int n)
+{
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	// int tid = threadIdx.x;
+
+	T* data;
+	if (index < m)
+	{
+		data = input + index * n;
+	}
+
+	// mean:
+	T mean_sum = (T)0.0;
+	for (unsigned j = 0; j < n; ++j)
+	{
+		mean_sum += data[j];
+	}
+
+	T mean = mean_sum / n;
+
+	T std_sum = (T)0.0;
+	for (unsigned j = 0; j < n; ++j)
+	{
+		std_sum += (data[j] - mean) * (data[j] - mean);
+	}
+
+	T std = sqrtf(std_sum / n);
+
+	for (unsigned j = 0; j < n; ++j)
+	{
+		output[index * n + j] = g*(data[j]-mean)/(std+1e-6) + b;
+	}
+}
+
+
+// warp reduce:
+template <typename T>
+__device__ T warpReduce(T val)
+{
+	for (int offset = WARP_SIZE / 2; offset > 0; offset >>= 2)
+	{
+		val += __shfl_down_sync(0xffffffff, val, offset);
+	}
+
+	return __shfl_sync(0xffffffff, val, 0);
+}
+
+
+
+// 每个 warp 处理一行数据：
+template <typename T>
+__global__ void layerNorm_warp(T* input, T* output, T g, T b, int m, int n)
+{
+	// mean:
+	int warpid = threadIdx.x / WARP_SIZE;
+	int laneid = threadIdx.x % WARP_SIZE;
+	int bid = blockIdx.x;
+	int warp_num = blockDim.x / WARP_SIZE;
+
+	int row = bid * warp_num + warpid;
+	if(row < m)
+	{
+		T* inp = input + row * n;
+		T mean = (T)0.0;
+		for (int i = laneid; i < n; i += WARP_SIZE)
+		{
+			mean += inp[i];
+		}
+
+		mean = warpReduce<T>(mean);
+
+		mean = mean / n;
+
+		// __syncthreads();
+		
+		// std:
+		T std = (T)0.0;
+		for (int i = laneid; i < n; i += WARP_SIZE)
+		{
+			std += (inp[i] - mean) * (inp[i] - mean);
+		}
+
+		std = warpReduce<T>(std);
+
+		std = sqrtf(std / n);
+
+		// __syncthreads();
+		
+		// layernorm:
+		
+		for (int i = laneid; i < n; i += WARP_SIZE)
+		{
+			output[row*n+i] = g*(inp[i] - mean) / (std + 1e-6) + b;
+		}
+
+	}
+
+}
+//
+//// block reduce
+//__global__ void layerNorm_kernel_1(float* input, float* output, float g, float b, int m, int n)
+//{
+//
+//
+//}
+//
+//// shared 加速 reduce操作：
+//// reduce + vec4
+//__global__ void layerNorm_kernel_3(float* input, float* output, float g, float b, int m, int n)
+//{
+//
+//
+//}
+
+
+
+
+int main()
+{
+	// cpu:
+	float* input, * output;
+	float g, b;
+
+	size_t data_bytes = sizeof(float) * (M * N);
+
+	input = (float*)malloc(data_bytes);
+	output = (float*)malloc(data_bytes);
+
+	for (unsigned i = 0; i < M; ++i)
+	{
+		for (unsigned j = 0; j < N; ++j)
+		{
+			input[i * N + j] = static_cast<float>(rand()) / RAND_MAX;
+		}
+	}
+
+	g = 0.1;
+	b = 0.1;
+	cpu_layernorm(input, output, g, b, M, N);
+
+	//for (unsigned i = 0; i < 20; ++i)
+	//{
+	//	printf("\n");
+	//	for (unsigned j = 0; j < 10; ++j)
+	//	{
+	//		printf("%.6f %s", output[i*N+j], "  ");
+	//	}
+	//}
+
+	// gpu
+	float* gpu_output;
+	gpu_output = (float*)malloc(data_bytes);
+
+	float* d_input, * d_output;
+	CHECK(cudaMalloc((void**)&d_input, data_bytes));
+	CHECK(cudaMalloc((void**)&d_output, data_bytes));
+
+	CHECK(cudaMemcpy(d_input, input, data_bytes, cudaMemcpyHostToDevice));
+
+	//size_t grid_x = (M + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	//dim3 grid_size(grid_x);
+	//dim3 block_size(BLOCK_SIZE);
+	//layerNorm_kernel<<<grid_size, block_size >>>(d_input, d_output, g, b, M, N);
+
+	size_t grid_x = (M + (BLOCK_SIZE/WARP_SIZE) - 1) / (BLOCK_SIZE/WARP_SIZE);
+	dim3 grid_size(grid_x);
+	dim3 block_size(BLOCK_SIZE);
+	layerNorm_warp<<<grid_size, block_size>>>(d_input, d_output, g, b, M, N);
+
+	cudaMemcpy(gpu_output, d_output, data_bytes, cudaMemcpyDeviceToHost);
+
+	// check:
+	bool error = false;
+	for (unsigned i = 0; i < M; ++i)
+	{
+		for (unsigned j = 0; j < N; ++j)
+		{
+			if (fabs(gpu_output[i * N + j] - output[i * N + j]) > 1e-6)
+			{
+				error = true;
+			}
+		}
+	}
+
+	printf("result: %s \n", error ? "fail" : "pass");
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			printf("gpu: %.8f, cpu: %.8f \n", gpu_output[i * N + j], output[i * N + j]);
+		}
+	}
+
+
+
+	free(input);
+	free(output);
+	free(gpu_output);
+	cudaFree(d_input);
+	cudaFree(d_output);
+
+	return 0;
+}
--- reduceBase/kernel.cu
+++ reduceBase/kernel.cu
@@ -6,7 +6,7 @@
 
 
 #define M 1024
-#define N 768
+#define N 2048
 
 #define WARP_SIZE 32
 #define BLOCK_SIZE 256
@@ -44,8 +44,10 @@ __device__ T warpReduce(T val)
 
 
 // 每个warp归约一行数据：
+// 1. 将数据先累加到一个warp
+// 2. 进行 warp reduce
 template <typename T>
-__global__ void matrixRowSum(T* input, T* output, int m, int n)
+__global__ void matrixRowSum_warp(T* input, T* output, int m, int n)
 {
 	int warpid = threadIdx.x / WARP_SIZE;
 	int laneid = threadIdx.x % WARP_SIZE;
@@ -54,7 +56,7 @@ __global__ void matrixRowSum(T* input, T* output, int m, int n)
 	int row = blockIdx.x * warp_num + warpid;
 	if (row < m)
 	{
-		T* inp = input + blockIdx.x * warp_num * n;
+		T* inp = input + row * n;
 
 		T sum = (T)0.0f;
 		for (int i = laneid; i < n; i += WARP_SIZE)
@@ -69,6 +71,64 @@ __global__ void matrixRowSum(T* input, T* output, int m, int n)
 			output[row] = sum;
 	}
 
+}
+
+
+// 一个block归约一行数据
+// 1. 先将数据累加到一个 block
+// 2. 进行 block reduce (基于warp reduce)
+// 3. 汇总多个 warp 的结果
+
+template <typename T>
+__device__ T blockReduce(T val)
+{
+	int warpid = threadIdx.x / WARP_SIZE;
+	int laneid = threadIdx.x % WARP_SIZE;
+
+	int warp_num = blockDim.x / WARP_SIZE;
+
+	val = warpReduce<T>(val);
+
+	__shared__ T s_warp_sum[32];
+
+	if (laneid == 0)
+		s_warp_sum[warpid] = val;
+
+	__syncthreads();
+
+	T sum = (T)0.0f;
+	if (warpid == 0)
+	{
+		for (int i = 0; i < warp_num; ++i)
+			sum += s_warp_sum[i];
+	}
+
+	return sum;
+}
+
+
+
+template <typename T>
+__global__ void matrixRowSum_block(T* input, T* output, int m, int n)
+{
+	int bid = blockIdx.x;
+	int tid = threadIdx.x;
+
+	int row = bid;
+	if (row < m)
+	{
+		T* inp = input + row * n;
+		T sum = (T)0.0f;
+		for (int i = tid; i < n; i += blockDim.x)
+		{
+			sum += inp[i];
+		}
+
+		sum = blockReduce<T>(sum);
+
+		if (tid == 0)
+			output[bid] = sum;
+	}
 }
 
 
@@ -100,12 +160,15 @@ int main()
 
 	cudaMemcpy(d_input, input, input_bytes, cudaMemcpyHostToDevice);
 
-	int grid_x = (M + BLOCK_SIZE - 1) / BLOCK_SIZE;
-	printf("1111111111grid_x: %d \n", grid_x);
+	//int grid_x = (M + (BLOCK_SIZE/WARP_SIZE) - 1) / (BLOCK_SIZE/WARP_SIZE);
+	//dim3 grid_size(grid_x);
+	//dim3 block_size(BLOCK_SIZE);
+	//matrixRowSum_warp<<<grid_size, block_size>>>(d_input, d_output, M, N);
+
+	int grid_x = M;
 	dim3 grid_size(grid_x);
 	dim3 block_size(BLOCK_SIZE);
-
-	matrixRowSum<<<grid_size, block_size>>>(d_input, d_output, M, N);
+	matrixRowSum_block<<<grid_size, block_size >>>(d_input, d_output, M, N);
 
 	cudaMemcpy(gpu_output, d_output, output_bytes, cudaMemcpyDeviceToHost);
 
