f4045a34c87a455742bf8be0a7bca918eac1efd6
simplefly2 <1419020389@qq.com>
2024-09-06 00:08:51 +0800

    add matrixMul kernel

--- MatrixMul/kernel.cu
+++ MatrixMul/kernel.cu
@@ -1,121 +1,145 @@
-﻿
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
 #include "cuda_runtime.h"
-#include "device_launch_parameters.h"
 
-#include <stdio.h>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+#define CHECK(func) do \
+{ \
+	cudaError_t err = func; \
+	if(err != cudaSuccess) \
+	{ \
+		printf("error: %s, %d \n", cudaGerErrorString(err), __LINE__); \
+	} \
+} while(0);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+
+#define BLOCK_SIZE 256
+
+#define M 1024
+#define N 768
+#define K 2048
+
+
+/*
+* A: M x N;
+* B: N x K;
+* 
+* A * B --> C (M, K)
+* 
+*/
+
+
+template <typename T>
+void cpu_matrixMul(T * A, T * B, T* C, int m, int n, int k)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < k; ++j)
+		{
+			T sum = 0.0f;
+			for (int t = 0; t < n; ++t)
+
+				sum += A[i * n + t] * B[t * k + j];
+			
+			C[i * k + j] = sum;
+		}
+	}
 }
+
+
+template <typename T>
+__global__ void marixMulKernrl0(T* A, T* B, T* C, int m, int n, int k)
+{
+
+
+
+
+}
+
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	// cpu:
+	float* A, *B, *C;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	size_t A_bytes = sizeof(float) * M * N;
+	size_t B_bytes = sizeof(float) * N * K;
+	size_t C_bytes = sizeof(float) * M * K;
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	A = (float*)malloc(A_bytes);
+	B = (float*)malloc(B_bytes);
+	C = (float*)malloc(C_bytes);
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < N; ++j)
 
-    return 0;
-}
+			A[i * N + j] = static_cast<float>(rand() % 10);
+	}
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < K; ++j)
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+			B[i * K + j] = static_cast<float>(rand() % 10);
+	}
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	cpu_matrixMul(A, B, C, M, N, K);
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	// gpu;
+	float* gpu_C;
+	float* d_A, * d_B, * d_C;
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	cudaMalloc((void**)&d_A, A_bytes);
+	cudaMalloc((void**)&d_B, B_bytes);
+	cudaMalloc((void**)&d_C, C_bytes);
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+	cudaMemcpy(d_A, A, A_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, B, B_bytes, cudaMemcpyHostToDevice);
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+	
+	int grid_x = (K + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	int grid_y = (M + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	dim3 grid_size(grid_x, grid_y);
+	dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
+	marixMulKernrl0<<<grid_size, block_size>>>(d_A, d_B, d_C, M, N, K);
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	cudaMemcpy(gpu_C, d_C, C_bytes, cudaMemcpyDeviceToHost);
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+
+	// check:
+	bool error = false;
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < K; ++j)
+		{
+			if (fabs(gpu_C[i * K + j] - C[i * K + j]) > 1e-6)
+
+				error = true;
+		}
+	}
+
+
+	printf("result: %s \n", error ? "fail" : "pass");
+
+
+	int offset = 100;
+	for (int i = 0; i < 20; ++i)
+	{
+		printf("cpu value: %.6f, gpu value: %.6f \n", C[offset + i], d_C[offset+i]);
+	}
+
+
+	free(A);
+	free(B);
+	free(C);
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+
+	return 0;
+
 }
